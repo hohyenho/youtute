fed9e9cd764939c5355ffbfad4db8f1733c48df4
hohyenho <75921113+hohyenho@users.noreply.github.com>
2025-02-05 17:12:13 +0900

    6_섹션 컴퍼넌트 구조화_header_main_footer

--- src/App.js
+++ src/App.js
@@ -10,28 +10,37 @@ import Gsap from './assets/page/Gsap';
 import Port from './assets/page/Port';
 import Youtube from './assets/page/Youtube';
 import Channel from './assets/page/Channel';
-
 import Search from './assets/page/Search';
 import Video from './assets/page/Video';
+
+import Header from './assets/components/section/Header';
+import Main from './assets/components/section/Main';
+import Footer from './assets/components/section/Footer';
+
+
 
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/today' element={<Today />} />
-                <Route path='/developer' element={<Developer />} />
-                <Route path='/web' element={<Web />} />
-                <Route path='/website' element={<Website />} />
-                <Route path='/gsap' element={<Gsap />} />
-                <Route path='/port' element={<Port />} />
-                <Route path='/youtube' element={<Youtube />} />
-                <Route path='/channel/:channelId' element={<Channel />} />
-                <Route path='/video/:videoId' element={<Video />} />
-                <Route path='/search/:searchId' element={<Search />} />
-                <Route path='*' element={<Not />} />
-            </Routes>
+            <Header />
+            <Main>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/today' element={<Today />} />
+                    <Route path='/developer' element={<Developer />} />
+                    <Route path='/web' element={<Web />} />
+                    <Route path='/website' element={<Website />} />
+                    <Route path='/gsap' element={<Gsap />} />
+                    <Route path='/port' element={<Port />} />
+                    <Route path='/youtube' element={<Youtube />} />
+                    <Route path='/channel/:channelId' element={<Channel />} />
+                    <Route path='/video/:videoId' element={<Video />} />
+                    <Route path='/search/:searchId' element={<Search />} />
+                    <Route path='*' element={<Not />} />
+                </Routes>
+            </Main>
+            <Footer />
         </BrowserRouter>
     )
 }
---
+++ src/assets/components/section/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id='footer' rel='contentinfo'>
+        footer
+    </footer>
+  )
+}
+
+export default Footer
